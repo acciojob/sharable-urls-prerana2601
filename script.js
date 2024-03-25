0b0d38b829d973fe92208ecf82bffe401bea4a5a
prerana2601 <155650129+prerana2601@users.noreply.github.com>
2024-03-25 19:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+// This is a JavaScript comment
+// JavaScript code starts executing from here
+
+// Define a variable
+var message = "Hello, world!";
+
+// Display the message in the console
+console.log(message);
+
+// Perform some arithmetic operations
+var a = 5;
+var b = 3;
+var sum = a + b;
+var product = a * b;
+
+// Display the results
+console.log("Sum:", sum);
+console.log("Product:", product);
+
